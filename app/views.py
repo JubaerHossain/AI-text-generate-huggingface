4de8a4ce291234847a403f40e0207660ff4534d1
JubaerHossain <jubaer01.cse@gmail.com>
2023-03-30 19:07:01 +0600

    text generate using hggingface

---
+++ app/views.py
@@ -0,0 +1,27 @@
+import json
+
+from django.http import JsonResponse
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from transformers import AutoModelWithLMHead, AutoTokenizer, pipeline, set_seed
+
+# Load pre-trained GPT-2 tokenizer and model
+tokenizer = AutoTokenizer.from_pretrained("gpt2")
+model = AutoModelWithLMHead.from_pretrained("gpt2")
+
+# Define a pipeline for generating text
+text_generator = pipeline("text-generation", model=model, tokenizer=tokenizer)
+
+# Set seed for reproducibility
+set_seed(42)
+
+@api_view(['POST'])
+def prompt_data(request):
+    prompt = request.data.get('prompt')
+    generated_text = text_generator(prompt, max_length=50, do_sample=True)[0]['generated_text']
+
+    return JsonResponse({
+        'answer': generated_text,        
+    }, status=status.HTTP_200_OK)
+    
